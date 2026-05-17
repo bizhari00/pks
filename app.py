e00edd918df1e391bbad2b7cadcb48d00c13e478
bizhari00 <bustamiizhari@gmail.com>
2026-05-17 11:32:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -65,12 +65,12 @@ process_phases = [
     [
         {
             'label': 'Laju Penerimaan TBS Kebun Sendiri',
-            'x': 140, 'y': 234,
+            'x': 150, 'y': 234,
             'tank_area': [177, 121, 295, 210]
         },
         {
             'label': 'Laju Penerimaan TBS Mitra',
-            'x': 120, 'y': 449,
+            'x': 150, 'y': 449,
             'tank_area': [192, 485, 332, 610]
         }
     ],
